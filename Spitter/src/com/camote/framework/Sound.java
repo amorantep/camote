af3ff528e928884a9e5b95936592ef53464bd185
amorantep <amorante16@gmail.com>
2014-10-10 16:01:33 -0500

    Commit

---
+++ Spitter/src/com/camote/framework/Sound.java
@@ -0,0 +1,7 @@
+package com.camote.framework;
+
+public interface Sound {
+	public void play(float volume);
+
+    public void dispose();
+}
---
+++ Spitter/src/com/camote/spitter/SpitterGame.java
@@ -0,0 +1,5 @@
+package com.camote.spitter;
+
+public class SpitterGame {
+
+}
